78b44c555d774512f3e9f8f102c1e84007c2f488
Tina Nguyen <nguyentinaca@yahoo.com>
2021-01-10 23:37:17 -0800

    gif

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
+$(document).ready(function () {
 //display date 
-$("#currentDay").text(moment().format("MMMM Do, YYYY"));
+$("#currentDay").text(moment().format("MMMM Do, YYYY, h:mm a"));
 
-  
+//save inputs
 $(".btn").on("click", function () {
        
     var input = $(this).siblings(".todo").val(); 
     var time = $(this).parent().attr("id"); 
 
-        //set items in local storage.
-        localStorage.setItem(time, input);
-    })
+    //save input in local storage
+    localStorage.setItem(time, input);
+})
     
-    $("#8Am .todo").val(localStorage.getItem("8Am"));
-    $("#9Am .todo").val(localStorage.getItem("9Am"));
-    $("#10Am .todo").val(localStorage.getItem("10Am"));
-    $("#11Am .todo").val(localStorage.getItem("11Am"));
-    $("#12Pm .todo").val(localStorage.getItem("12Pm"));
-    $("#1Pm .todo").val(localStorage.getItem("1Pm"));
-    $("#2Pm .todo").val(localStorage.getItem("2Pm"));
-    $("#3Pm .todo").val(localStorage.getItem("3Pm"));
-    $("#4Pm .todo").val(localStorage.getItem("4Pm"));
-    $("#5Pm .todo").val(localStorage.getItem("5Pm"));
+// show the save input 
+    $("#8Hour .todo").val(localStorage.getItem("8Hour"));
+    $("#9Hour .todo").val(localStorage.getItem("9Hour"));
+    $("#10Hour .todo").val(localStorage.getItem("10Hour"));
+    $("#11Hour .todo").val(localStorage.getItem("11Hour"));
+    $("#12Hour .todo").val(localStorage.getItem("12Hour"));
+    $("#1Hour .todo").val(localStorage.getItem("1Hour"));
+    $("#2Hour .todo").val(localStorage.getItem("2Hour"));
+    $("#3Hour .todo").val(localStorage.getItem("3Hour"));
+    $("#4Hour .todo").val(localStorage.getItem("4Hour"));
+    $("#5Hour .todo").val(localStorage.getItem("5Hour"));
+
+//show past, present, future schedule color coordinate
+function timeList () {
+    
+    var currentHour = moment().hour();
+    
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var scheduleHour = parseInt($(this).attr("id").split("Hour")[1]);
+        console.log(scheduleHour, currentHour)
+
+        //use past css (grey) for past hours
+        if (scheduleHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        //use present css (red) for current hour
+        else if (scheduleHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        //use future css (green) for future hours
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+//rerun function
+timeList();
+})
